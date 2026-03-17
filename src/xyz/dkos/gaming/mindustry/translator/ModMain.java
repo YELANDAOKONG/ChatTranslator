aa7b109ecc5729c7b30c9e1d60ec2161470fefd2
YELANDAOKONG <116365019+YELANDAOKONG@users.noreply.github.com>
2026-03-17 16:38:23 +0800

    Refactor main class and update entry point

---
+++ src/xyz/dkos/gaming/mindustry/translator/ModMain.java
@@ -0,0 +1,16 @@
+package xyz.dkos.gaming.mindustry.translator;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+public class ModMain extends Mod {
+
+    public ModMain(){
+        Log.info("Chat Translator Loaded.");
+    }
+}
